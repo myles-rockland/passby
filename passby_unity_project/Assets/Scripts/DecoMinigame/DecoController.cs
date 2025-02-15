d1fe53d59be32a00ce14fc1818510a2bafd4479e
Myles Rockland <myles.rockland@students.plymouth.ac.uk>
2025-02-15 15:35:38 +0000

    Add DecoMinigame scene + controller obj

---
+++ passby_unity_project/Assets/Scripts/DecoMinigame/DecoController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PassBy
+{
+    public class DecoController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
